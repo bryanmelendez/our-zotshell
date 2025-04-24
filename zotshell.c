2991912938ea2d13fbad9653ecddacf6d20e756a
Matthew Quach <matthewquach@Matthews-MacBook-Pro-2.local>
2025-04-24 12:58:21 -0700

    part 5 pipe

--- zotshell.c
+++ zotshell.c
@@ -14,6 +14,9 @@ int main(void)
     char input[MAX_LINE];
     char *args[MAX_LINE/2 + 1] = {NULL}; /* command line arguments */
 
+    char *args1[MAX_LINE/2 + 1] = {NULL}; // before pipe
+    char *args2[MAX_LINE/2 + 1] = {NULL}; // after pipe
+
     int should_run = 1; /* flag to determine when to exit program */
     char *history[MAX_LINE/2 + 1] = {NULL};
 
@@ -21,6 +24,7 @@ int main(void)
     while (should_run) {
         int arg_num = 0;
         int fork_flag = 1; //initialize to fork
+        int pipe_flag = 0;
         pid_t pid = 0;
 
         printf("osh>");
@@ -36,21 +40,48 @@ int main(void)
             continue;
         }
 
-        args[arg_num] = strtok(input, " ");
+        // get pointer of pipe separation |
+        char *pipe_location = strchr(input, '|');
 
-        while (args[arg_num] != NULL) {
-            arg_num++;
-            args[arg_num] = strtok(NULL, " ");
+        if (pipe_location != NULL) {
+            pipe_flag = 1;
+            *pipe_location = '\0';
+            pipe_location++;
+            while (*pipe_location == ' ') {
+                pipe_location++;
+            }
+            // get first command
+            args1[arg_num] = strtok(input, " ");
+            while (args1[arg_num] != NULL) {
+                arg_num++;
+                args1[arg_num] = strtok(NULL, " ");
+            }
+            args1[++arg_num] = NULL; // terminates the argument list
+            // get second command
+            arg_num = 0;
+            args2[arg_num] = strtok(pipe_location, " ");
+            while (args2[arg_num] != NULL) {
+                arg_num++;
+                args2[arg_num] = strtok(NULL, " ");
+            }
+            args2[++arg_num] = NULL; // terminates the argument list
         }
-        args[++arg_num] = NULL; // terminates the argument list
+        else { // parse like normal
+            args[arg_num] = strtok(input, " ");
+            while (args[arg_num] != NULL) {
+                arg_num++;
+                args[arg_num] = strtok(NULL, " ");
+            }
+            args[++arg_num] = NULL; // terminates the argument list
+        }
 
         // If statements to handle the exit condition
-        if(strcmp(args[0], "exit") == 0) {
+        if(!pipe_flag && args[0] != NULL && strcmp(args[0], "exit") == 0) {
             // If the command is exit flag should_run and not fork
             should_run = 0;
             fork_flag = 0;
         }
-        else if(strcmp(args[0], "!!") == 0)
+        else if(!pipe_flag && args[0] != NULL && strcmp(args[0], "!!") == 0)
         {
             if (history[0] == NULL) {
                 printf("No commands in history\n");
@@ -101,45 +132,77 @@ int main(void)
             }
         }
 
-        if (fork_flag) {
+        if (pipe_flag) {
+            int fd[2]; // fd[0] = read, fd[1] = write
+            if (pipe(fd) == -1) {
+                printf("error with opening pipe");
+            }
+            pid_t pid1 = fork(); // first
+            if (pid1 == 0) { // child process 1
+                close(fd[0]);
+                dup2(fd[1], STDOUT_FILENO);
+                close(fd[1]);
+                execvp(args1[0], args1);
+            } else if (pid1 < 0) {
+                perror("fork for first command");
+                continue;
+            }
+            
+            pid_t pid2 = fork(); // second
+            if (pid2 == 0) { // child process 2
+                close(fd[1]);
+                dup2(fd[0], STDIN_FILENO);
+                close(fd[0]);
+                execvp(args2[0], args2);
+            } else if (pid2 < 0) {
+                perror("fork for second command");
+                continue;
+            }
+            // parent
+            close(fd[0]);
+            close(fd[1]);
+            waitpid(pid1, NULL, 0);
+            waitpid(pid2, NULL, 0);
+        }
+        else if (fork_flag) {
             pid = fork();
-        }
+            // I got the following from the slides
+            if (pid < 0) { // if an error occurs
+                fprintf(stderr, "Fork failed, terminating program");
+                return 1;
+            }
+            else if (pid == 0) { // child process
 
-        // I got the following from the slides
-        if (pid < 0) { // if an error occurs
-            fprintf(stderr, "Fork failed, terminating program");
-            return 1;
-        }
-        else if (pid == 0) { // child process
+            if (redirection==1) {
+                int fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644); //open or create file
+                dup2(fd, STDOUT_FILENO);
+                close(fd);
+            }
+            else if (redirection==2) {
+                int fd = open(file, O_RDONLY); //open or create file
+                dup2(fd, STDIN_FILENO);
+                close(fd);
+            }
 
-        if (redirection==1) {
-            int fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644); //open or create file
-            dup2(fd, STDOUT_FILENO);
-            close(fd);
-        }
-        else if (redirection==2) {
-            int fd = open(file, O_RDONLY); //open or create file
-            dup2(fd, STDIN_FILENO);
-            close(fd);
-        }
-
-            execvp(args[0], args);
-            //memcpy(history, args, MAX_LINE/2 + 1);
+                execvp(args[0], args);
+                //memcpy(history, args, MAX_LINE/2 + 1);
 
 
-            if (errno == ENOENT) {
-                fprintf(stderr, "%s: command not found\n", args[0]);
-                exit(1);
-            } else {
-                fprintf(stderr, "%s: %s\n", args[0], strerror(errno));
-                exit(1);
+                if (errno == ENOENT) {
+                    fprintf(stderr, "%s: command not found\n", args[0]);
+                    exit(1);
+                } else {
+                    fprintf(stderr, "%s: %s\n", args[0], strerror(errno));
+                    exit(1);
+                }
+            }
+            else { // parent process
+                wait(NULL);
+                // we only need to wait if user does not put &
+                // in the command
             }
         }
-        else { // parent process
-            wait(NULL);
-            // we only need to wait if user does not put &
-            // in the command
-        }
+    
 
         // flush input and output just in case something weird is still in there
         fflush(stdout);
